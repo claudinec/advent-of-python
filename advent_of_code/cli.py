39fdfe8694eb793a28e93fc4fe8fe3dfd2d61798
Claudine Chionh <git@claudinec.net>
2022-11-29 23:19:29 +1100

    choose the year

--- advent_of_code/cli.py
+++ advent_of_code/cli.py
@@ -4,18 +4,24 @@ from datetime import datetime, timezone, timedelta
 
 import click
 
-AOC_YEAR = 2022
+AOC_MIN_YEAR = 2015
 AOC_TZ = timezone(timedelta(hours=-5))
 LOCAL_TZ = timezone(timedelta(hours=11))
 
+local_dt = datetime.now(tz=LOCAL_TZ)
+current_year = local_dt.strftime("%Y")
+
 @click.command()
-def main():
+@click.option("--aoc-year", type=click.IntRange(AOC_MIN_YEAR, current_year))
+def main(aoc_year):
     """Placeholder main function."""
-    aoc_start = datetime(AOC_YEAR, 12, 1, 0, 0, 0, tzinfo=AOC_TZ)
-    local_dt = datetime.now(tz=LOCAL_TZ)
-    click.echo("Advent of code " + str(AOC_YEAR))
-    if local_dt < aoc_start:
-        click.echo("Too early")
+    aoc_start = datetime(aoc_year, 12, 1, 0, 0, 0, tzinfo=AOC_TZ)
+    click.echo("Advent of code " + str(aoc_year))
+    if local_dt < aoc_start and aoc_year == current_year:
+        click.echo("Too early, try a previous year")
+    else:
+        aoc_dir = "aoc" + str(aoc_year)
+        # check or create directory
 
 if __name__ == "__main__":
     main()
