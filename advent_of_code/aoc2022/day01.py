4dff8ec23cf3ebac1aecf5402ce5f1c9909cc907
Claudine Chionh <git@claudinec.net>
2022-12-02 22:17:00 +1100

    renaming for clarity

--- advent_of_code/aoc2022/day01.py
+++ advent_of_code/aoc2022/day01.py
@@ -2,24 +2,24 @@
 
 import os
 
-def cal_groups(input_dir: str) -> list:
+def cals_groups(data_suffix: str) -> list:
     inventory = list()
-    input_file = os.path.join(input_dir, 'day01.txt')
-    with open(input_file, 'r', encoding='utf-8') as cal_input:
-        cals = 0
-        for line in cal_input:
-            cal_int = line.strip()
-            if cal_int.isdecimal():
-                cals = cals + int(cal_int)
+    data_file = os.path.join('data', f'day01-{data_suffix}.txt')
+    with open(data_file, 'r', encoding='utf-8') as cals_input:
+        cals_int = 0
+        for line in cals_input:
+            cals_str = line.strip()
+            if cals_str.isdecimal():
+                cals_int = cals_int + int(cals_str)
             else:
-                inventory.append(cals)
-                cals = 0
-        inventory.append(cals)
+                inventory.append(cals_int)
+                cals_int = 0
+        inventory.append(cals_int)
     return inventory
 
-for x in ['sample', 'input']:
-    print(x)
-    groups = cal_groups(x)
+for suffix in ['example', 'input']:
+    print(suffix)
+    groups = cals_groups(suffix)
 
     ## Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
     print('Part 1')
@@ -28,5 +28,5 @@ for x in ['sample', 'input']:
     ## Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
     print('Part 2')
     groups_sorted = sorted(groups, reverse=True)
-    top_three = groups_sorted[0:3]
+    top_three = groups_sorted[:3]
     print(sum(top_three))
