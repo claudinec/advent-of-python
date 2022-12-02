2c59b184a3b8d13c2fe53cf24947152b29501a63
Claudine Chionh (claudine) <git@claudinec.net>
2022-12-02 18:47:55 +1100

    hacky day 1 complete

--- aoc2022/day01.py
+++ aoc2022/day01.py
@@ -1,9 +1,8 @@
-# Day 1a
+# Day 1
 
 import os
 
-## Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-def max_cals(input_dir: str) -> int:
+def cal_groups(input_dir: str) -> list:
     inventory = list()
     input_file = os.path.join(input_dir, 'day01.txt')
     with open(input_file, 'r', encoding='utf-8') as cal_input:
@@ -16,8 +15,20 @@ def max_cals(input_dir: str) -> int:
                 inventory.append(cals)
                 cals = 0
         inventory.append(cals)
-    return max(inventory)
+    return inventory
 
+## Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+print('Part 1')
 for x in ['sample', 'input']:
     print(x)
-    print(max_cals(x))
+    groups = cal_groups(x)
+    print(max(groups))
+
+## Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+print('Part 2')
+for x in ['sample', 'input']:
+    print(x)
+    groups = cal_groups(x)
+    groups_sorted = sorted(groups, reverse=True)
+    top_three = groups_sorted[0:3]
+    print(sum(top_three))
