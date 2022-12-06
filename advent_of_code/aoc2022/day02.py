d4f392b88429b937ae5b6090821dbeb4a7a6b56f
Claudine Chionh (claudine) <git@claudinec.net>
2022-12-06 12:50:29 +1100

    consolidate function

--- advent_of_code/aoc2022/day02.py
+++ advent_of_code/aoc2022/day02.py
@@ -2,30 +2,22 @@
 
 import os
 
-# The first column is what your opponent is going to play: A for Rock,
-# B for Paper, and C for Scissors.
-
-# The second column, you reason, must be what you should play in response:
-# X for Rock, Y for Paper, and Z for Scissors.
-
-# Your total score is the sum of your scores for each round. The score for
-# a single round is the score for the shape you selected (1 for Rock,
-# 2 for Paper, and 3 for Scissors) plus the score for the outcome of
-# the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
-
 SCORING_DICT = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+OUTCOME_DICT = {'X': 0, 'Y': 3, 'Z': 6}
 
-def rps_score(data_suffix: str) -> int:
-    print(data_suffix)
+def rps_score(data_suffix: str):
+    print(data_suffix.upper())
     data_file = os.path.join('data', f'day02-{data_suffix}.txt')
     total_score = 0
-    # One round per line.
+    # Part One
+    # The first column is what your opponent is going to play: A for Rock,
+    # B for Paper, and C for Scissors.
     with open(data_file, 'r', encoding='utf8') as rps_guide:
         for line in rps_guide:
             rps_round = line.strip()
             [their_choice, my_choice] = rps_round.split(' ')
-            # Score for my choice.
-            choice_score = SCORING_DICT[my_choice]
+            # The second column, you reason, must be what you should play in response:
+            # X for Rock, Y for Paper, and Z for Scissors.
             # Did I win?
             match [their_choice, my_choice]:
                 case ['A', 'Y'] | ['B', 'Z'] | ['C', 'X']:
@@ -34,12 +26,18 @@ def rps_score(data_suffix: str) -> int:
                     win_score = 3
                 case ['A', 'Z'] | ['B', 'X'] | ['C', 'Y']:
                     win_score = 0
-            # Score for the round.
-            round_score = choice_score + win_score
+            # Your total score is the sum of your scores for each round. The score for
+            # a single round is the score for the shape you selected (1 for Rock,
+            # 2 for Paper, and 3 for Scissors) plus the score for the outcome of
+            # the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+            round_score =  SCORING_DICT[my_choice] + win_score
             # Total score.
             total_score = total_score + round_score
-    print(total_score)
-    return total_score
+            # Part Two
+            # The second column says how the round needs to end:
+            # X means you need to lose, Y means you need to end the round in a draw,
+            # and Z means you need to win.
+    print(f'Total score (part one): {total_score}\n')
 
 for suffix in ['example', 'input']:
     rps_score(suffix)
