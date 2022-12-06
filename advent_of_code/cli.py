ad1da3e842772a7e1725ede6ab04667ac269d573
Claudine Chionh (claudine) <git@claudinec.net>
2022-12-06 18:58:33 +1100

    paths for running code

--- advent_of_code/cli.py
+++ advent_of_code/cli.py
@@ -6,12 +6,12 @@ from pathlib import Path
 import click
 
 AOC_MIN_YEAR = 2015
+AOC_MAX_DAY = 25
 AOC_TZ = timezone(timedelta(hours=-5))
 LOCAL_TZ = timezone(timedelta(hours=11))
 
 local_dt = datetime.now(tz=LOCAL_TZ)
 current_year = int(local_dt.strftime("%Y"))
-AOC_MAX_DAY = 25
 aoc_current_start = datetime(current_year, 12, 1, 0, 0, 0, tzinfo=AOC_TZ)
 aoc_current_end = datetime(current_year, 12, 25, 0, 0, 0, tzinfo=AOC_TZ)
 if local_dt < aoc_current_start:
@@ -53,24 +53,29 @@ def list_years():
     for year in range(AOC_MIN_YEAR, aoc_max_year+1):
         click.echo(year)
 
-# @main.command()
-# @click.option("--aoc-year", type=click.IntRange(AOC_MIN_YEAR, aoc_max_year), default=aoc_max_year, show_default=True)
-# @click.option("--aoc-day", type=click.IntRange(1, AOC_MAX_DAY), default=AOC_MAX_DAY, show_default=True)
-# def run(aoc_year: int, aoc_day: int):
+@main.command()
+@click.option("--aoc-year", type=click.IntRange(AOC_MIN_YEAR, aoc_max_year), default=aoc_max_year, show_default=True)
+@click.option("--aoc-day", type=click.IntRange(1, AOC_MAX_DAY), default=AOC_MAX_DAY, show_default=True)
+def run(aoc_year: int, aoc_day: int):
     # Run code for given year and day.
     # If not provided, set puzzle year to current year.
-    # if aoc_year is None:
-    #     aoc_year = current_year
-    # click.echo("Advent of code " + str(aoc_year))
-    # if aoc_year == current_year and local_dt < aoc_current_start:
-    #     click.echo("Too early, try a previous year")
-    # else:
-    #     # Work out puzzle day if not provided.
-    #     if aoc_day is None:
-    #         if aoc_year == current_year:
-    #             aoc_day = aoc_current_max_day
-    #         else:
-    #             aoc_day = AOC_MAX_DAY
+    if aoc_year is None:
+        aoc_year = current_year
+    if aoc_year == current_year and local_dt < aoc_current_start:
+        click.echo("Too early, try a previous year", err=True)
+    else:
+        aoc_dir = f'aoc{aoc_year:04d}'
+        aoc_path = Path(aoc_dir)
+        # Work out puzzle day if not provided.
+        if aoc_day is None:
+            if aoc_year == current_year:
+                aoc_day = aoc_current_max_day
+            else:
+                aoc_day = AOC_MAX_DAY
+        day_str = f'day{aoc_day:02d}'
+        code_path = aoc_path.joinpath(f'{day_str}.py')
+        data_path = aoc_path.joinpath('data')
+        data_dirs = [data_path.joinpath(f'{day_str}-example.txt'), data_path.joinpath(f'{day_str}-input.txt')]
 
 if __name__ == "__main__":
     main()
