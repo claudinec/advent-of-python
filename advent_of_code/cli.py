31e828f503bdc6fd6941b67524e056875d676600
Claudine Chionh <info@claudinec.net>
2023-08-01 17:41:26 +1000

    capitalise constants

--- advent_of_code/cli.py
+++ advent_of_code/cli.py
@@ -1,4 +1,5 @@
-"""Main entrypoint for Advent of Code exercises in Python."""
+"""Main command-line interface for Advent of Code exercises in Python.
+"""
 
 from datetime import datetime, timezone, timedelta
 from pathlib import Path
@@ -9,26 +10,25 @@ AOC_MIN_YEAR = 2015
 AOC_MAX_DAY = 25
 AOC_TZ = timezone(timedelta(hours=-5))
 LOCAL_TZ = timezone(timedelta(hours=11))
-
-local_dt = datetime.now(tz=LOCAL_TZ)
-current_year = int(local_dt.strftime("%Y"))
-aoc_current_start = datetime(current_year, 12, 1, 0, 0, 0, tzinfo=AOC_TZ)
-aoc_current_end = datetime(current_year, 12, 25, 0, 0, 0, tzinfo=AOC_TZ)
-if local_dt < aoc_current_start:
-    aoc_max_year = current_year - 1
+LOCAL_DT = datetime.now(tz=LOCAL_TZ)
+CURRENT_YEAR = LOCAL_DT.year
+AOC_CURRENT_BEGIN = datetime(CURRENT_YEAR, 12, 1, 0, 0, 0, tzinfo=AOC_TZ)
+AOC_CURRENT_END = datetime(CURRENT_YEAR, 12, 25, 0, 0, 0, tzinfo=AOC_TZ)
+if LOCAL_DT < AOC_CURRENT_BEGIN:
+    AOC_MAX_YEAR = CURRENT_YEAR - 1
 else:
-    aoc_max_year = current_year
-    if local_dt >= aoc_current_end:
-        aoc_current_max_day = 25
+    AOC_MAX_YEAR = CURRENT_YEAR
+    if LOCAL_DT >= AOC_CURRENT_END:
+        AOC_CURRENT_MAX_DAY = 25
     else:
-        aoc_current_max_day = int(local_dt.strftime("%d"))
+        AOC_CURRENT_MAX_DAY = int(LOCAL_DT.strftime("%d"))
 
 @click.group()
 def main():
     click.echo('Advent of Code')
 
 @main.command()
-@click.option("--aoc-year", type=click.IntRange(AOC_MIN_YEAR, aoc_max_year), default=aoc_max_year, show_default=True)
+@click.option("--aoc-year", type=click.IntRange(AOC_MIN_YEAR, AOC_MAX_YEAR), default=AOC_MAX_YEAR, show_default=True)
 def create(aoc_year: int):
     # Create directory structure for given year.
     aoc_dir = f'aoc{aoc_year:04d}'
